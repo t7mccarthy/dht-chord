1cf95975b496ee6cec7f9d037572335c3f7460bf
Tom McCarthy <thomasmccarthy@college.harvard.edu>
2019-12-07 10:29:28 -0800

    working on kademlia

--- kademlia/run_sim.py
+++ kademlia/run_sim.py
@@ -25,4 +25,18 @@ print('RUNNING SIMULATION:')
 
 # command = ['python3 start_network.py']
 
-subprocess.run(['python3 start_network.py'])
+# subprocess.run(['python3', 'start_network.py', 'python3', 'new_node.py'])
+# # print("Done")
+# subprocess.run(['python3', 'new_node.py'])
+# subprocess.run(['python3', 'new_node.py'])
+# subprocess.run(['python3', 'new_node.py'])
+# subprocess.run(['python3', 'new_node.py'])
+# subprocess.run(['python3', 'new_node.py'])
+N=4
+print(f"This will open {N} gnome terminals.")
+command = ['gnome-terminal','--tab','-e',"python3 start_network.py"]
+for i in range(N-1):
+    new_gnome = ['--tab','-e',"python3 new_node.py 0.0.0.0 8468"]
+    command.extend(new_gnome)
+print('This is the comamnd:', command)
+subprocess.run(command)
